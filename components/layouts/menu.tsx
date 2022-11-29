6a4da01e939c722453f23ed5d7dec3176b596d09
jarangit <jaran.dch@gmail.com>
2022-11-30 00:17:13 +0700

    feat: create menu

--- components/layouts/menu.tsx
+++ components/layouts/menu.tsx
@@ -4,9 +4,17 @@ type Props = {}
 
 const Menu = (props: Props) => {
   return (
-    <div className='bg-red'>
-      <div>
-        content
+    <div className='bg-red h-24'>
+      <div className='myContainer'>
+        <div>
+          <div>logo</div>
+          <div>ระบบบริหารจัดการเว็บเพจสมาชิกแสตมป์ไทย</div>
+          <div>
+            <div>iconuser</div>
+            <div>เบญจพร ทองมูลตน</div>
+            <div>arrow icon</div>
+          </div>
+        </div>
       </div>
     </div>
   )
